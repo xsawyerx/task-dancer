3b2187baffacb52d332ac55061d08885cd44a6c8
sawyer <sawyer@dungeon.(none)>
2010-02-18 17:37:37 +0200

    first commit

---
+++ lib/Task/Dancer.pm
@@ -0,0 +1,99 @@
+package Task::Dancer;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Task::Dancer - Dancer in a box
+
+=head1 VERSION
+
+Version 0.01
+
+=head1 DESCRIPTION
+
+This Task installs Dancer and optional Dancer engines and assorted modules that are not included in the Dancer core distribution.
+
+If you've written anything relating to Dancer, please let me know.
+
+=head1 Template Engines
+
+=head2 L<Dancer::Template::Tiny>
+
+=head2 L<Dancer::Template::Tenjin>
+
+=head2 L<Dancer::Template::HAML>
+
+=head2 L<Dancer::Template::MicroTemplate>
+
+=head1 Logging Engines
+
+=head2 L<Dancer::Logger::Syslog>
+
+=head2 L<Dancer::Logger::LogHandler>
+
+=head1 Session Engines
+
+=head2 L<Dancer::Session::Memcache>
+
+=head2 L<Dancer::Session::Cookie>
+
+=head1 AUTHOR
+
+Sawyer X, C<< <xsawyerx at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-task-dancer at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Task-Dancer>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Task::Dancer
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Task-Dancer>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Task-Dancer>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Task-Dancer>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Task-Dancer/>
+
+=back
+
+=head1 ACKNOWLEDGEMENTS
+
+L<Dancer> team.
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2010 Sawyer X.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+

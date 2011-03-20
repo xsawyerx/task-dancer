0728a2d8ba66613704878eb0bc3bbce34fb4e266
Alberto Simoes <ambs@cpan.org>
2011-03-20 18:22:04 +0000

    0.25, no big changes

--- lib/Task/Dancer.pm
+++ lib/Task/Dancer.pm
@@ -1,6 +1,6 @@
 package Task::Dancer;
 
-our $VERSION = '0.24';
+our $VERSION = '0.25';
 
 1;
 
@@ -12,7 +12,7 @@ Task::Dancer - Dancer in a box
 
 =head1 VERSION
 
-Version 0.24
+Version 0.25
 
 =head1 DESCRIPTION
 

f719791175227f60323a43cb28f2a325738af233
ambs <ambs@cpan.org>
2011-08-11 14:11:28 +0100

    bumped version number

--- lib/Task/Dancer.pm
+++ lib/Task/Dancer.pm
@@ -1,6 +1,6 @@
 package Task::Dancer;
 
-our $VERSION = '0.30';
+our $VERSION = '0.31';
 
 1;
 
@@ -12,7 +12,7 @@ Task::Dancer - Dancer in a box
 
 =head1 VERSION
 
-Version 0.30
+Version 0.31
 
 =head1 DESCRIPTION
 
